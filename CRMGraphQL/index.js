13dab397a55a4b7c2257644ed28f67ed079d7d38
Marcos R <mriveros.py@gmail.com>
2022-04-04 21:47:32 -0400

    modified:   index.js

--- CRMGraphQL/index.js
+++ CRMGraphQL/index.js
@@ -1,17 +1,39 @@
 const { ApolloServer, gql } = require('apollo-server');
 
+const cursos = [
+    {
+        titulo: 'JavaScript Moderno Guía Definitiva Construye +10 Proyectos',
+        tecnologia: 'JavaScript ES6',
+    },
+    {
+        titulo: 'React – La Guía Completa: Hooks Context Redux MERN +15 Apps',
+        tecnologia: 'React',
+    },
+    {
+        titulo: 'Node.js – Bootcamp Desarrollo Web inc. MVC y REST API’s',
+        tecnologia: 'Node.js'
+    }, 
+    {
+        titulo: 'ReactJS Avanzado – FullStack React GraphQL y Apollo',
+        tecnologia: 'React'
+    }
+];
 // The GraphQL schema
 const typeDefs = gql`
-  type Query {
-    "A simple type for getting started!"
-    hello: String
+type Curso{
+    titulo: String
+    tecnologia: String
+}  
+type Query {
+    obtenerCursos: Curso
   }
+  
 `;
 
 // A map of functions which return data for the schema.
 const resolvers = {
   Query: {
-    hello: () => 'world',
+    obtenerCursos: () => {cursos[0]},
   },
 };
 
