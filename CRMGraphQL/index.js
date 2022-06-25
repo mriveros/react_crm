a310a2b401526d4adceaa0681ebcadb459c3215d
Marcos R <mriveros.py@gmail.com>
2022-06-25 18:10:27 -0400

    modified:   index.js

--- CRMGraphQL/index.js
+++ CRMGraphQL/index.js
@@ -1,47 +1,53 @@
 const { ApolloServer, gql } = require('apollo-server');
 
-const cursos = [
-    {
-        titulo: 'JavaScript Moderno Guía Definitiva Construye +10 Proyectos',
-        tecnologia: 'JavaScript ES6',
-    },
-    {
-        titulo: 'React – La Guía Completa: Hooks Context Redux MERN +15 Apps',
-        tecnologia: 'React',
-    },
-    {
-        titulo: 'Node.js – Bootcamp Desarrollo Web inc. MVC y REST API’s',
-        tecnologia: 'Node.js'
-    }, 
-    {
-        titulo: 'ReactJS Avanzado – FullStack React GraphQL y Apollo',
-        tecnologia: 'React'
-    }
-];
+
 // The GraphQL schema
 const typeDefs = gql`
 type Curso{
     titulo: String
     tecnologia: String
 }  
+type Tecnologia{
+  tecnologia: String
+}
 type Query {
-    obtenerCursos: Curso
+    obtenerCursos: [Curso]
+    obtenerTecnologia: [Tecnologia]
   }
   
 `;
 
+const cursos = [
+  {
+      titulo: 'JavaScript Moderno Guía Definitiva Construye +10 Proyectos',
+      tecnologia: 'JavaScript ES6',
+  },
+  {
+      titulo: 'React – La Guía Completa: Hooks Context Redux MERN +15 Apps',
+      tecnologia: 'React',
+  },
+  {
+      titulo: 'Node.js – Bootcamp Desarrollo Web inc. MVC y REST API’s',
+      tecnologia: 'Node.js'
+  }, 
+  {
+      titulo: 'ReactJS Avanzado – FullStack React GraphQL y Apollo',
+      tecnologia: 'React'
+  }
+];
 // A map of functions which return data for the schema.
 const resolvers = {
   Query: {
-    obtenerCursos: () => {cursos[0]},
-  },
+    obtenerCursos: () => cursos,
+    obtenerTecnologia: () => cursos
+  }
 };
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers,
+  resolvers, 
 });
 
 server.listen().then(({ url }) => {
-  console.log(`🚀 Server ready at ${url}`);
+  console.log(`🚀 Servidor listo en la url: ${url}`);
 });
