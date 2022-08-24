952cf8831b112cbd5d5d3d81ee7bb01cfffcf4ee
Marcos R <mriveros.py@gmail.com>
2022-08-23 22:06:41 -0400

    modified:   CRMGraphQL/db/resolvers.js 	modified:   CRMGraphQL/db/schema.js

--- CRMGraphQL/db/resolvers.js
+++ CRMGraphQL/db/resolvers.js
@@ -3,7 +3,7 @@
   // A map of functions which return data for the schema.
   const resolvers = {
     Query: {
-     
+     obtenerCurso: () => "algo"
     }
   };
   module.exports = resolvers;
--- CRMGraphQL/db/schema.js
+++ CRMGraphQL/db/schema.js
@@ -1,6 +1,8 @@
 const { gql } = require('apollo-server');
 // The GraphQL schema
 const typeDefs = gql`
-
+  type Query{
+    obtenerCurso: String
+  }
 `;
 module.exports = typeDefs;
