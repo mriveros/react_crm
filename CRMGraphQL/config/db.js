5ef2f5ec1a8ca2e6867289bff70c001cecf1ce20
Marcos R <mriveros.py@gmail.com>
2022-08-24 22:02:05 -0400

    Changes to be committed: 	modified:   config/db.js 	deleted:    config/variables_env 	modified:   index.js 	new file:   variables.env

---
+++ CRMGraphQL/config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require ('mongoose');
+require ('dotenv').config({path: 'variables.env'});
+
+const conectarDB = async() =>{
+ try {
+      await mongoose.connect(process.env.DB_MONGO,{
+        
+      });
+      console.log('DB conectada');
+ } catch (error) {
+    console.log('Hubo un error');
+    console.log(error);
+    process.exit(1); //este codigo detiene la aplicacion
+ }
+}
+module.exports = conectarDB;
--- CRMGraphQL/index.js
+++ CRMGraphQL/index.js
@@ -2,8 +2,9 @@ const { ApolloServer, gql } = require('apollo-server');
 const { ApolloServerPluginLandingPageGraphQLPlayground } = require('apollo-server-core');
 const typeDefs = require('./db/schema');
 const resolvers = require('./db/resolvers');
-
-
+const conectarDB = require('./config/db');
+//conectar a la base de datos
+conectarDB();
 
 const server = new ApolloServer({
   typeDefs,
