8d0d0f7262bd2844e0065fc4ae32b59b3d2c80bf
Marcos R <mriveros.py@gmail.com>
2022-08-31 17:33:31 -0400

    modified:   db/resolvers.js 	modified:   db/schema.js

--- CRMGraphQL/db/resolvers.js
+++ CRMGraphQL/db/resolvers.js
@@ -1,4 +1,4 @@
-
+const Usuario = require('../models/Usuario');
 
 // A map of functions which return data for the schema.
 const resolvers = {
@@ -6,9 +6,26 @@ const resolvers = {
     obtenerCurso: () => "algo"
   },
   Mutation: {
-    nuevoUsuario: (_, { input }) => {
-      console.log(input);
-      return "Creando...";
+    nuevoUsuario: async (_, { input }) => {
+
+      const { email, password } = input;
+
+      //revisar si el usuario ya esta registrado
+      const existeUsuario = await Usuario.findOne({ email });
+      console.log(existeUsuario);
+      if (existeUsuario) {
+        throw new Error('El usuario ya esta registrado.');
+      }
+      //hash de password
+
+      //guardar en la base de datos
+      try {
+        const usuario = new Usuario(input);
+        usuario.save();
+        return usuario;
+      } catch (error) {
+        console.log(error);
+      }
 
     }
   }
