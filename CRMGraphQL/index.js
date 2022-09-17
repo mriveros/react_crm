af3612caa77348ecf3e5e70c7b2ba7d008d7cbb0
Marcos R <mriveros.py@gmail.com>
2022-09-16 23:05:43 -0400

    modified:   index.js

--- CRMGraphQL/index.js
+++ CRMGraphQL/index.js
@@ -3,6 +3,9 @@ const { ApolloServerPluginLandingPageGraphQLPlayground } = require('apollo-serve
 const typeDefs = require('./db/schema');
 const resolvers = require('./db/resolvers');
 const conectarDB = require('./config/db');
+const jwt = require('jsonwebtoken');
+require('dotenv').config({ path: 'variables.env' });
+
 //conectar a la base de datos
 conectarDB();
 
@@ -10,8 +13,18 @@ const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: ({ req }) => {
-    console.log(req.headers['authorization']);
-    const token = req.headers['authorization']
+    //console.log(req.headers['authorization']);
+    const token = req.headers['authorization'] || ' ';
+    if (token) {
+      try {
+        const usuario = jwt.verify(token, process.env.SECRETA);
+        console.log(usuario);
+      } catch (error) {
+        console.log('Hubo un error:')
+        console.log(error);
+      }
+    }
+
   },
   plugins: [
     ApolloServerPluginLandingPageGraphQLPlayground({
