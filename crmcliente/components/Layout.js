746cf81519d00369c09b59137795bac18b8bb4fd
Marcos R <mriveros.py@gmail.com>
2022-10-27 23:09:51 -0300

    modified:   components/Layout.js

--- crmcliente/components/Layout.js
+++ crmcliente/components/Layout.js
@@ -1,9 +1,14 @@
 import React from 'react';
-
+import Head from 'next/head'
 const Layout = ({ children }) => {
 
     return (
         <>
+            <head>
+                <title>CRM -  Administracion de Clientes</title>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css" integrity="sha512-oHDEc8Xed4hiW6CxD7qjbnI+B07vDdX7hEPTvn9pSZO1bcRqHp8mj9pyr+8RVC2GmtEfI2Bi9Ke9Ass0as+zpg==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
+                <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet" />
+            </head>
             <h1>Desde Layout</h1>
             {children}
         </>
