93b475eb1567916fe7181b2799e1835343c47abb
Marcos Riveros <mriveros.py@gmail.com>
2022-11-02 14:10:38 -0300

    modified:   pages/login.js

--- crmcliente/pages/login.js
+++ crmcliente/pages/login.js
@@ -6,6 +6,20 @@ const Login = () =>{
         <>
         <Layout>
         <h1 className="text-center text-2xl text-white font-ligth">Login</h1>
+        <div className="flex justify-center mt-5">
+            <div className="w-full max-w-sm">
+                <form
+                className="bg-white rounted shadow-md px-8 pt-6 pb-8 mb-4">
+                    <div>
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="nombre" >
+                            Email
+                        </label>
+                        <input 
+                        />
+                    </div>
+                </form>
+            </div>
+        </div>
         </Layout>
         </>
     );
