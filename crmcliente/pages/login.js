de0f0c55947b15f98c8ef7a5ab88dd2d63b699ae
Marcos R <mriveros.py@gmail.com>
2022-11-02 22:35:01 -0300

    modified:   pages/login.js

--- crmcliente/pages/login.js
+++ crmcliente/pages/login.js
@@ -1,26 +1,43 @@
 import React from "react";
 import Layout from "../components/Layout";
 
-const Login = () =>{
+const Login = () => {
     return (
         <>
-        <Layout>
-        <h1 className="text-center text-2xl text-white font-ligth">Login</h1>
-        <div className="flex justify-center mt-5">
-            <div className="w-full max-w-sm">
-                <form
-                className="bg-white rounted shadow-md px-8 pt-6 pb-8 mb-4">
-                    <div>
-                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="nombre" >
-                            Email
-                        </label>
-                        <input 
-                        />
+            <Layout>
+                <h1 className="text-center text-2xl text-white font-ligth">Login</h1>
+                <div className="flex justify-center mt-5">
+                    <div className="w-full max-w-sm">
+                        <form
+                            className="bg-white rounted shadow-md px-8 pt-6 pb-8 mb-4">
+                            <div className="mb-4">
+                                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email" >
+                                    Email
+                                </label>
+                                <input className="shadow appearance-none border rounded w-full py-2 px3 text-gray-700 leading-tigth focus:outline-none focus:shadow-outline"
+                                    id="email"
+                                    type="email"
+                                    placeholder="Email Usuario"
+                                />
+                            </div>
+                            <div className="mb-4">
+                                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password" >
+                                    Password
+                                </label>
+                                <input className="shadow appearance-none border rounded w-full py-2 px3 text-gray-700 leading-tigth focus:outline-none focus:shadow-outline"
+                                    id="password"
+                                    type="password"
+                                    placeholder="Password Usuario"
+                                />
+                            </div>
+                            <input
+                                type="submit" className="bg-gray-800 w-full mt-5 p-2 text-white uppercase hover:bg-gray-700"
+                                value="Iniciar Sesión"
+                            />
+                        </form>
                     </div>
-                </form>
-            </div>
-        </div>
-        </Layout>
+                </div>
+            </Layout>
         </>
     );
 }
