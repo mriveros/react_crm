feb89498ba7996d758decc7fc501e1bf678d6de4
Marcos R <mriveros.py@gmail.com>
2022-11-21 22:36:50 -0300

    modified:   pages/index.js

--- crmcliente/pages/index.js
+++ crmcliente/pages/index.js
@@ -5,6 +5,7 @@ import { gql, useQuery } from '@apollo/client'
 const OBTENER_CLIENTES_USUARIO = gql`
 query obtenerClientesVendedor{
   obtenerClientesVendedor{
+    id
     nombre
     apellido
     empresa
@@ -20,12 +21,36 @@ const Index = () => {
   console.log(data);
   console.log(loading);
   console.log(error);
+
+  if (loading) return 'Cargando...';
+
   return (
     <div>
       <Layout>
         <h1 className='text-2xl text-gray-800 font-light'>
           Clientes
         </h1>
+        <table className='table-auto shadow-md mt-10 w-full w-lg'>
+          <thead className='bg-gray-800'>
+            <tr className='text-white'>
+              <th className='w-1/5 py-2'>Nombre</th>
+              <th className='w-1/5 py-2'>Empresa</th>
+              <th className='w-1/5 py-2'>Email</th>
+
+            </tr>
+
+          </thead>
+          <tbody className='bg-white'>
+            {data.obtenerClientesVendedor.map(cliente => (
+              <tr key={cliente.id}>
+                clientes
+              </tr>
+
+            ))}
+
+          </tbody>
+
+        </table>
       </Layout>
     </div>
   )
