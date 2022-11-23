bd191ffa5c51e2a9c7e52d3d61dfb26c84ba4962
Marcos R <mriveros.py@gmail.com>
2022-11-22 22:32:23 -0300

    new file:   components/Header.js 	modified:   components/Layout.js

---
+++ crmcliente/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className='flex justify-end'>
+            <h1 className='mr-2'>Hola: Marcos</h1>
+            <button type='button'>Cerrar Sesión</button>
+        </div>
+    );
+}
+
+export default Header;
