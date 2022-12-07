57f22b759bef10795de823c3bd987fb4e4559f6b
Marcos R <mriveros.py@gmail.com>
2022-12-06 22:07:12 -0300

    modified:   pages/nuevocliente.js

--- crmcliente/pages/nuevocliente.js
+++ crmcliente/pages/nuevocliente.js
@@ -3,6 +3,28 @@ import Layout from '../components/Layout';
 const NuevoCliente = () => {
     return (<Layout>
         <h1 className='text-2xl text-gray-800 font-light'>Nuevo Cliente</h1>
+        <div className='flex justify-center mt-5'>
+            <div className='w-full max-w-lg'>
+                <form className='bg-white shadow-md px-8 pt-6 pb-8 mb-4'>
+                    <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email" >
+                            Email
+                        </label>
+                        <input className="shadow appearance-none border rounded w-full py-2 px3 text-gray-700 leading-tigth focus:outline-none focus:shadow-outline"
+                            id="email"
+                            type="email"
+                            placeholder="Email Usuario"
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            value={formik.values.email}
+                        />
+                    </div>
+
+                </form>
+
+            </div>
+
+        </div>
     </Layout>)
 }
 
